344361553e6f03a78186dcb4d079fbbc3fc26b89
deepaka <akash.deep@adidas.com>
2023-08-25 14:32:01 +0530

    first commit

---
+++ src/main/java/com/example/javahacks/JavahacksApplication.java
@@ -0,0 +1,13 @@
+package com.example.javahacks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavahacksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavahacksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javahacks/JavahacksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javahacks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavahacksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
